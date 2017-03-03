201be222026f7918aaac2ecea40934514388f454
Fanilo Andrianasolo <andfanilo@gmail.com>
2017-03-03 17:09:06 +0100

    Solved Thrift problem, interpreter just echoes your message for now =)

--- src/main/java/com/github/andfanilo/giphy/GiphyInterpreter.java
+++ src/main/java/com/github/andfanilo/giphy/GiphyInterpreter.java
@@ -37,7 +37,7 @@ public class GiphyInterpreter extends Interpreter {
 
     @Override
     public FormType getFormType() {
-        return null;
+        return FormType.SIMPLE;
     }
 
     @Override
